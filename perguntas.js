fd1b42883200a549a43dcb0b439082014628f101
isabellegusman <00001114388968sp@al.educacao.sp.gov.br>
2024-11-26 15:26:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Premios',
+    'Qual filme recebeu o maior número de Oscars até hoje?',
+    'Três filmes estão empatados com 11 Oscars: Ben-Hur, Titanic e O Senhor Dos Aneis.'
+)
+
+criaCartao(
+    'Sucesso',
+    'Qual o primeiro livro da franquia Harry Potter?',
+    'O titulo do primeiro filme é Harry Potter e a Pedra Filosofal (2001)'
+)
+
+criaCartao(
+    'Super-Herois',
+    'Qual é o verdadeiro nome do Homem de Ferro?',
+    'Seu nome verdadeiro é Tony Stark'
+)
+
+criaCartao(
+    'Cinema Nacional',
+    'Qual filme conta a história do Capitão Nascimento e o BOPE?',
+    'O filme brasileiro Topa de Elite (2007)'
+)
